c5af1f085be8cc25b9d642b30ea5559b84d68501
KholdFuzion <kholdfuzion@gmail.com>
2026-08-09 07:49:43 -0400

    build version and time fixes

--- examples/companion_radio/MyMesh.h
+++ examples/companion_radio/MyMesh.h
@@ -8,11 +8,11 @@
 #define FIRMWARE_VER_CODE 13
 
 #ifndef FIRMWARE_BUILD_DATE
-#define FIRMWARE_BUILD_DATE "6 Jun 2026"
+#define FIRMWARE_BUILD_DATE __DATE__
 #endif
 
 #ifndef FIRMWARE_VERSION
-#define FIRMWARE_VERSION "v1.16.0"
+#define FIRMWARE_VERSION "v1.16.x"
 #endif
 
 #if defined(NRF52_PLATFORM) || defined(STM32_PLATFORM)
